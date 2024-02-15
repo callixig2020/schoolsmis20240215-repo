e5b112aa48e08f6373adbc78747299107e89e382
callixig <callixig@gmail.com>
2024-02-15 05:16:30 -0800

    changes in index file
    added boostrap link

--- src/index.php
+++ src/index.php
@@ -1,3 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SchoolMIS</title>
+</head>
+<body>
+    
+</body>
+</html>
+
 <?php
 echo"<h3> We are dealing with PHP</H3>";
 />
